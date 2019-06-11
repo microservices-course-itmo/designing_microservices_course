d8ba6964f5d8c704f9e209c87cc01ef172bf84fd
ehlinashin <ehlinashin@yandex.ru>
2019-06-11 22:50:10 +0300

    Spring boot hello, world created

---
+++ service1/src/main/java/com/microservices/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.microservices.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hiiii!";
+    }
+
+}
