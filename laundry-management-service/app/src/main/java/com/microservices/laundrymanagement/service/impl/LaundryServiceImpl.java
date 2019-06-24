15be6843857ab109be828a2fa14ccc23a54a9575
ehlinashin <ehlinashin@yandex.ru>
2019-06-24 23:55:29 +0300

    Add logger for laundry creating

--- laundry-management-service/app/src/main/java/com/microservices/laundrymanagement/service/impl/LaundryServiceImpl.java
+++ laundry-management-service/app/src/main/java/com/microservices/laundrymanagement/service/impl/LaundryServiceImpl.java
@@ -3,12 +3,16 @@ package com.microservices.laundrymanagement.service.impl;
 import com.microservices.laundrymanagement.entity.LaundryStateEntity;
 import com.microservices.laundrymanagement.repository.LaundryStateRepository;
 import com.microservices.laundrymanagement.service.LaundryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LaundryServiceImpl implements LaundryService {
     private LaundryStateRepository laundryStateRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(LaundryServiceImpl.class);
 
     @Autowired
     public LaundryServiceImpl(LaundryStateRepository laundryStateRepository) {
@@ -17,8 +21,12 @@ public class LaundryServiceImpl implements LaundryService {
 
     @Override
     public int addNewLaundry() {
+        logger.info("Creating queue...");
+
         LaundryStateEntity laundryStateEntity = new LaundryStateEntity();
-        LaundryStateEntity id = laundryStateRepository.save(laundryStateEntity);
-        return id.getId();
+        int id = laundryStateRepository.save(laundryStateEntity).getId();
+
+        logger.info("Created queue with id {}", id);
+        return id;
     }
 }
