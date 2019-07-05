f9c0e170eca5d69b04fddac2e505f3754f3b4c3a
shine2 <elina.shin@emc.com>
2019-07-05 19:18:53 +0300

    Add spring boot annotation

--- tariff-management-service/app/src/main/java/com/microservices/tariffmanagement/TariffManagementService.java
+++ tariff-management-service/app/src/main/java/com/microservices/tariffmanagement/TariffManagementService.java
@@ -1,4 +1,11 @@
 package com.microservices.tariffmanagement;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class TariffManagementService {
+    public static void main(String[] args) {
+        SpringApplication.run(TariffManagementService.class, args);
+    }
 }
