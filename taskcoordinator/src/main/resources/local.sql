99ef31e6647d2ebdd22dd2af0cd29d42215fb46e
AfanasievYuriy <afanasiev.yuriy12@gmail.com>
2019-07-06 19:43:18 +0300

    change taskcoordinator project structure

--- taskcoordinator/src/main/resources/local.sql
+++
@@ -1,4 +0,0 @@
-DROP DATABASE IF EXISTS task_coordinator_db;
-DROP USER IF EXISTS task_coordinator_db_user;
-CREATE USER task_coordinator_db_user WITH ENCRYPTED PASSWORD '123';
-CREATE DATABASE task_coordinator_db WITH OWNER task_coordinator_db_user;
