39227db3a25c78d8947a09cf602a0f0dc4321e4d
darksidersstrife <vvllaadd99@yandex.ru>
2019-07-08 21:43:38 +0300

    Deleted serealizer and changed type to byte array, also removed bean annotation

--- laundry-management-service/app/src/main/java/com/microservices/laundrymanagement/kafka/producer/MessageSenderConfig.java
+++ laundry-management-service/app/src/main/java/com/microservices/laundrymanagement/kafka/producer/MessageSenderConfig.java
@@ -1,8 +1,7 @@
 package com.microservices.laundrymanagement.kafka.producer;
 
-import com.microservices.laundrymanagement.kafka.message.OrderSubmittedMessageSerializer;
-import com.microservices.laundrymanagementapi.messages.OrderSubmittedEvent;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,17 +19,16 @@ public class MessageSenderConfig {
     @Value(value = "${kafka.bootstrapAddress}")
     private String bootstrapAddress;
 
-    @Bean
-    public ProducerFactory<String, OrderSubmittedEvent.OrderSubmittedMessage> producerFactory() {
+    public ProducerFactory<String, byte[]> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, OrderSubmittedMessageSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<String, OrderSubmittedEvent.OrderSubmittedMessage> kafkaTemplate() {
+    public KafkaTemplate<String, byte[]> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 
