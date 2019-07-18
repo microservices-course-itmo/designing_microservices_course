d56d14a4a658834688591c4e15e40169a01b3a16
shinehlina <ehlinashin@yandex.ru>
2019-07-18 23:45:57 +0300

    add input DTO validation (#30)

--- taskcoordinator/app/src/main/java/com/microservices/taskcoordinator/dto/inbound/OrderProcessedDto.java
+++ taskcoordinator/app/src/main/java/com/microservices/taskcoordinator/dto/inbound/OrderProcessedDto.java
@@ -3,17 +3,28 @@ package com.microservices.taskcoordinator.dto.inbound;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @EqualsAndHashCode
+@NoArgsConstructor
 @AllArgsConstructor
 public class OrderProcessedDto {
 
+    @NotNull
     Integer orderId;
 
+    @Valid
+    @NotNull
     InboundLaundryStateDto laundryState;
 
+    @Min(value = 0L, message = "Order completion time must be positive")
+    @NotNull
     Long completionTime;
 }
