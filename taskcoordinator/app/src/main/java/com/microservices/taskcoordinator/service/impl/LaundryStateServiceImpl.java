4e3b9ee0775ef87cc41f93abf681602be82ca8c4
ehlinashin <ehlinashin@yandex.ru>
2019-07-18 15:47:20 +0300

    limit number of returning dtos

--- taskcoordinator/app/src/main/java/com/microservices/taskcoordinator/service/impl/LaundryStateServiceImpl.java
+++ taskcoordinator/app/src/main/java/com/microservices/taskcoordinator/service/impl/LaundryStateServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
