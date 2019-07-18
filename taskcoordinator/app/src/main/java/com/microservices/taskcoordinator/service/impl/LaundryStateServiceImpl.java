c2b9abb7ae71018186cae262a87d1828c75c185a
ehlinashin <ehlinashin@yandex.ru>
2019-07-18 16:03:53 +0300

    minor fix ˆ

--- taskcoordinator/app/src/main/java/com/microservices/taskcoordinator/service/impl/LaundryStateServiceImpl.java
+++ taskcoordinator/app/src/main/java/com/microservices/taskcoordinator/service/impl/LaundryStateServiceImpl.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Objects;
 
 @Service
