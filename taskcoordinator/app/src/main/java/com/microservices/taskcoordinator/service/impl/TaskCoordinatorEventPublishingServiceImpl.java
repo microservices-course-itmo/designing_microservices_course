5502455cf1db9bd2a717fa79750c8c59c79eb7a5
yuriy <afanasiev.yuriy12@gmail.com>
2019-07-22 02:39:45 +0300

    rename class

--- taskcoordinator/app/src/main/java/com/microservices/taskcoordinator/service/impl/TaskCoordinatorEventPublishingServiceImpl.java
+++ taskcoordinator/app/src/main/java/com/microservices/taskcoordinator/service/impl/TaskCoordinatorEventPublishingServiceImpl.java
@@ -17,12 +17,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class LaundryEventPublishingServiceImpl implements TaskCoordinatorEventPublishingService {
+public class TaskCoordinatorEventPublishingServiceImpl implements TaskCoordinatorEventPublishingService {
 
     private TaskCoordinatorEventRepository taskCoordinatorEventRepository;
 
     @Autowired
-    public LaundryEventPublishingServiceImpl(TaskCoordinatorEventRepository taskCoordinatorEventRepository) {
+    public TaskCoordinatorEventPublishingServiceImpl(TaskCoordinatorEventRepository taskCoordinatorEventRepository) {
         this.taskCoordinatorEventRepository = taskCoordinatorEventRepository;
     }
 
