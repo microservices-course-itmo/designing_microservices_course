85ecac3e4ff290854078850d844c93ab009d3f50
ehlinashin <ehlinashin@yandex.ru>
2019-07-21 17:18:57 +0300

    fix id inconsistency in controller

--- taskcoordinator/app/src/main/java/com/microservices/taskcoordinator/controllers/CoordinatorController.java
+++ taskcoordinator/app/src/main/java/com/microservices/taskcoordinator/controllers/CoordinatorController.java
@@ -7,6 +7,7 @@ import com.microservices.taskcoordinator.dto.outbound.OrderSubmissionDto;
 import com.microservices.taskcoordinator.service.LaundryStateService;
 import com.microservices.taskcoordinator.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,18 +24,26 @@ public class CoordinatorController {
 
     private LaundryStateService laundryStateService;
 
+    private static final String INCONSISTENT_ID_ERROR_MESSAGE = "Order id in path and in DTO must be equal";
+
     @PostMapping
     OrderSubmissionDto coordinateOrder(@Valid @RequestBody OrderCoordinationDto orderCoordinationDTO) {
         return orderService.coordinateOrder(orderCoordinationDTO);
     }
 
     @PutMapping("/{id}/status/submitted")
-    void processSubmittedOrder(@Valid @RequestBody OrderSubmittedDto orderSubmittedDTO) {
+    void processSubmittedOrder(@Valid @RequestBody OrderSubmittedDto orderSubmittedDTO,
+                               @PathVariable Integer id) {
+        if (!id.equals(orderSubmittedDTO.getOrderId()))
+            throw new IllegalArgumentException(INCONSISTENT_ID_ERROR_MESSAGE);
         laundryStateService.updateLaundryStateWithOrderSubmitted(orderSubmittedDTO);
     }
 
     @PutMapping("/{id}/status/processed")
-    void processProcessedOrder(@Valid @RequestBody OrderProcessedDto orderProcessedDTO) {
+    void processProcessedOrder(@Valid @RequestBody OrderProcessedDto orderProcessedDTO,
+                               @PathVariable Integer id) {
+        if (!id.equals(orderProcessedDTO.getOrderId()))
+            throw new IllegalArgumentException(INCONSISTENT_ID_ERROR_MESSAGE);
         laundryStateService.updateLaundryStateWithOrderProcessed(orderProcessedDTO);
     }
 
