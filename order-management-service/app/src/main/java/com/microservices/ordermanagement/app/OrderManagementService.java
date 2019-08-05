1f0122b68a670954148aace9b4bd31eb77e09bfa
shine2 <elina.shin@emc.com>
2019-08-05 14:57:25 +0300

    add unfinished impl

--- order-management-service/app/src/main/java/com/microservices/ordermanagement/app/OrderManagementService.java
+++ order-management-service/app/src/main/java/com/microservices/ordermanagement/app/OrderManagementService.java
@@ -2,10 +2,12 @@ package com.microservices.ordermanagement.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableFeignClients("com.microservices.tariffmanagement.api.feign")
 public class OrderManagementService {
 
     public static void main(String[] args) {
