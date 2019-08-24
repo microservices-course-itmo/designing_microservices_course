7a3af0865058ba43430d1ab0f75f95521da66d1a
RusskovMatvey <45367590+RusskovMatvey@users.noreply.github.com>
2019-08-24 13:37:09 +0300

    up services in docker containers

---
+++ taskcoordinator/taskcoordinator-app/src/main/java/com/microservices/taskcoordinator/dto/inbound/OrderCoordinationDto.java
@@ -0,0 +1,40 @@
+package com.microservices.taskcoordinator.dto.inbound;
+
+import com.microservices.ordermanagement.api.events.OrderCreatedEventWrapper.OrderCreatedEvent;
+import com.microservices.taskcoordinator.dto.OrderDetailDto;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OrderCoordinationDto {
+
+    @NotNull
+    private Integer orderId;
+
+    @NotNull
+    private List<OrderDetailDto> details;
+
+    public OrderCoordinationDto(OrderCreatedEvent orderCreatedEvent) {
+        this.orderId = orderCreatedEvent.getOrder().getOrderId();
+        this.details = orderCreatedEvent.getOrder().getDetailsList().stream()
+                .map(d -> new OrderDetailDto(
+                        d.getDetailId(),
+                        d.getWeight(),
+                        d.getDuration(),
+                        d.getOrderId()))
+                .collect(Collectors.toList());
+    }
+}
