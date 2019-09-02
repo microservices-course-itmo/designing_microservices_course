5920c64d98fab0ab9721061e758e6953571be1b2
MatveyRusskov <russkov_matvey@mail.ru>
2019-09-02 13:49:31 +0300

    fixed modelmapper in laundry service

--- laundry-management-service/laundrymanagement-app/src/main/java/com/microservices/laundrymanagement/configuration/OrderServiceBeansConfig.java
+++ laundry-management-service/laundrymanagement-app/src/main/java/com/microservices/laundrymanagement/configuration/OrderServiceBeansConfig.java
@@ -6,11 +6,9 @@ import com.microservices.laundrymanagement.entity.OrderEntity;
 import com.microservices.laundrymanagement.entity.OrderStatus;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.spi.MappingContext;
+import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
 
 @Configuration
 public class OrderServiceBeansConfig {
@@ -18,31 +16,27 @@ public class OrderServiceBeansConfig {
     @Bean
     ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration()
-                .setFieldMatchingEnabled(true)
-                .setFieldAccessLevel(PRIVATE);
 
-        //This style of code needed because otherwise the converter will not work
-        //toOrderEntityConverter needed because without it, a field mismatch error occurs
-        Converter<OrderSubmissionDto, OrderEntity> toOrderEntityConverter = new Converter<OrderSubmissionDto, OrderEntity>() {
+        modelMapper.addMappings(new PropertyMap<OrderSubmissionDto, OrderEntity>() {
             @Override
-            public OrderEntity convert(MappingContext<OrderSubmissionDto, OrderEntity> context) {
-                OrderSubmissionDto source = context.getSource();
-                OrderEntity destination = context.getDestination() == null ? new OrderEntity() : context.getDestination();
-                destination.setId(source.getOrderId());
-                destination.setSubmittedTime(System.currentTimeMillis());
-                destination.setEstimatedTime(source.getDetails().stream()
-                        .map(DetailSubmissionDto::getDuration)
-                        .reduce(0L, Long::sum));
-                destination.setStatus(OrderStatus.QUEUED);
-                destination.setLaundryId(source.getLaundryId());
-                destination.setBucket(source.getBucket());
-                return destination;
+            protected void configure() {
+                map().setId(source.getOrderId());
             }
-        };
-
-        modelMapper.addConverter(toOrderEntityConverter);
+        }).setPostConverter(toOrderEntityConverter);
 
         return modelMapper;
     }
+
+    private Converter<OrderSubmissionDto, OrderEntity> toOrderEntityConverter = context -> {
+        OrderSubmissionDto source = context.getSource();
+        OrderEntity destination = context.getDestination();
+
+        destination.setSubmittedTime(System.currentTimeMillis());
+        destination.setEstimatedTime(source.getDetails().stream()
+                .map(DetailSubmissionDto::getDuration)
+                .reduce(0L, Long::sum));
+        destination.setStatus(OrderStatus.QUEUED);
+
+        return context.getDestination();
+    };
 }
