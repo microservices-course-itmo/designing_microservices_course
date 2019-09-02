05d51edce7003b50a5c08b5ee58669e984ca4636
shine2 <elina.shin@emc.com>
2019-09-02 20:11:24 +0300

    ksksksksksksk

--- laundry-management-service/laundrymanagement-app/src/main/java/com/microservices/laundrymanagement/configuration/OrderServiceBeansConfig.java
+++ laundry-management-service/laundrymanagement-app/src/main/java/com/microservices/laundrymanagement/configuration/OrderServiceBeansConfig.java
@@ -3,12 +3,13 @@ package com.microservices.laundrymanagement.configuration;
 import com.microservices.laundrymanagement.dto.DetailSubmissionDto;
 import com.microservices.laundrymanagement.dto.OrderSubmissionDto;
 import com.microservices.laundrymanagement.entity.OrderEntity;
-import com.microservices.laundrymanagement.entity.OrderStatus;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
 
 @Configuration
 public class OrderServiceBeansConfig {
@@ -16,27 +17,25 @@ public class OrderServiceBeansConfig {
     @Bean
     ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(PRIVATE);
+
+        Converter<OrderSubmissionDto, OrderEntity> toOrderEntityPostConverter = context -> {
+            context.getDestination().setEstimatedTime(context.getSource().getDetails().stream()
+                    .map(DetailSubmissionDto::getDuration)
+                    .reduce(0L, Long::sum));
+
+            return context.getDestination();
+        };
 
         modelMapper.addMappings(new PropertyMap<OrderSubmissionDto, OrderEntity>() {
             @Override
             protected void configure() {
                 map().setId(source.getOrderId());
             }
-        }).setPostConverter(toOrderEntityConverter);
+        }).setPostConverter(toOrderEntityPostConverter);
 
         return modelMapper;
     }
-
-    private Converter<OrderSubmissionDto, OrderEntity> toOrderEntityConverter = context -> {
-        OrderSubmissionDto source = context.getSource();
-        OrderEntity destination = context.getDestination();
-
-        destination.setSubmittedTime(System.currentTimeMillis());
-        destination.setEstimatedTime(source.getDetails().stream()
-                .map(DetailSubmissionDto::getDuration)
-                .reduce(0L, Long::sum));
-        destination.setStatus(OrderStatus.QUEUED);
-
-        return context.getDestination();
-    };
 }
--- laundry-management-service/laundrymanagement-app/src/main/java/com/microservices/laundrymanagement/entity/OrderEntity.java
+++ laundry-management-service/laundrymanagement-app/src/main/java/com/microservices/laundrymanagement/entity/OrderEntity.java
@@ -1,14 +1,16 @@
 package com.microservices.laundrymanagement.entity;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @Entity
 @Table(name = "orders")
 public class OrderEntity {
@@ -16,12 +18,12 @@ public class OrderEntity {
     @Id
     private int id;
 
-    private long submittedTime;
+    private long submittedTime = System.currentTimeMillis();
 
     private long estimatedTime;
 
     @Enumerated(value = EnumType.STRING)
-    private OrderStatus status;
+    private OrderStatus status = OrderStatus.QUEUED;
 
     private int laundryId;
 
