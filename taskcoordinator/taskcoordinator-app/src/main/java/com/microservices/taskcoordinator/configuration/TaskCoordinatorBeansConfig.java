86e4bae62ce4a0f89ae3bfce028ef4de040f1754
MatveyRusskov <russkov_matvey@mail.ru>
2019-09-05 20:46:35 +0300

    remove configuration model mapper

--- taskcoordinator/taskcoordinator-app/src/main/java/com/microservices/taskcoordinator/configuration/TaskCoordinatorBeansConfig.java
+++ taskcoordinator/taskcoordinator-app/src/main/java/com/microservices/taskcoordinator/configuration/TaskCoordinatorBeansConfig.java
@@ -4,18 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
-
 @Configuration
 public class TaskCoordinatorBeansConfig {
 
     @Bean
     ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration()
-                .setFieldMatchingEnabled(true)
-                .setFieldAccessLevel(PRIVATE);
-
-        return modelMapper;
+        return new ModelMapper();
     }
 }
