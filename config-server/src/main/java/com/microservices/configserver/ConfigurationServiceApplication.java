90b68414da4cba2709657efb9a82fde159ddcbfa
shine2 <elina.shin@emc.com>
2019-09-08 20:32:10 +0300

    rename from "config" to "config-server"

--- config-server/src/main/java/com/microservices/configserver/ConfigurationServiceApplication.java
+++ config-server/src/main/java/com/microservices/configserver/ConfigurationServiceApplication.java
@@ -1,4 +1,4 @@
-package com.microservices.config;
+package com.microservices.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
