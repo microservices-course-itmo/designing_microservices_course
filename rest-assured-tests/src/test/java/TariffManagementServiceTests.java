e7e539cd75b6518cb2d3341bee254d1fac2235eb
aianushevich <a.ianushevich@gmail.com>
2019-09-09 17:02:30 +0300

    Hardcode to constants, javadoc adding

--- rest-assured-tests/src/test/java/TariffManagementServiceTests.java
+++ rest-assured-tests/src/test/java/TariffManagementServiceTests.java
@@ -6,6 +6,10 @@ import static org.junit.Assert.assertEquals;
 
 public class TariffManagementServiceTests {
     private static int tariffsNumber;
+    private final static String TARIFF_NAME = "NewTestTariff";
+    private final static float TARIFF_PRICE = 4.67f;
+    private final static int TARIFF_WASHING_TIME = 12;
+    private final static int DELTA = 0;
 
     @BeforeClass
     public static void configureRestAssured() {
@@ -13,16 +17,21 @@ public class TariffManagementServiceTests {
         tariffsNumber = TariffManagementServiceClass.getTariffsList().length;
     }
 
+    /**
+     * 1. Create new tariff using POST method.
+     * 2. Verify that number of tariffs has been increased by 1.
+     * 3. Get new tariff by id using GET method.
+     * 4. Verify that new tariff is the same as expected.
+     */
     @Test
     public void testNewTariffSuccessfulCreation() {
-        Tariff newTestTariff = TariffManagementServiceClass.createTariffByPost("NewTestTariff", 4.67f, 12);
-
+        Tariff newTestTariff = TariffManagementServiceClass.createTariffByPost(TARIFF_NAME, TARIFF_PRICE, TARIFF_WASHING_TIME);
         assertEquals(tariffsNumber + 1, TariffManagementServiceClass.getTariffsList().length);
         Tariff newTestTariffById = TariffManagementServiceClass.getTariffById(newTestTariff.id);
 
         assertEquals(newTestTariff.id, newTestTariffById.id);
         assertEquals(newTestTariff.name, newTestTariffById.name);
-        assertEquals(newTestTariff.price, newTestTariffById.price, 0);
+        assertEquals(newTestTariff.price, newTestTariffById.price, DELTA);
         assertEquals(newTestTariff.washingTime, newTestTariffById.washingTime);
     }
 }
